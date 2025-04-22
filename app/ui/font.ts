15559e3640d8a5435070db639e3a6a6f3f6c6262
NiKomangDesyWulandari-2551 <231712551@students.uajy.ac.id>
2025-04-22 19:54:13 +0700

    update.tsx

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,5 +1,6 @@
-import { Cinzel, Lacquer, Chilanka } from "next/font/google";
+import { Cinzel, Lacquer, Chilanka, Lusitana } from "next/font/google";
 
 export const cinzel = Cinzel({ subsets: ["latin"] });
 export const lacquer = Lacquer({ weight: "400", subsets: ["latin"] });
 export const chilanka = Chilanka({ weight: "400", subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
